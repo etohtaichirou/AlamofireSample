287c88f4922ec33d194cb17b3ca08ec15350d063
CTC Team <ctcdev@GM161104noMacBook-Pro.local>
2019-03-28 12:42:17 +0900

    Alamofire通信部分の実装

--- AlamofireSample/ViewController.swift
+++ AlamofireSample/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import RxSwift
 
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let disposeBag = DisposeBag()
+        WeblandApiManager.call(
+            TradeListRequest.get(
+                from: "20151",              // 取引時期From
+                to: "20151",                // 取引時期To
+                area: "13",                 // 都道府県コード
+                city: "13101",              // 市区町村コード
+                station: nil                // 駅コード
+            ),
+            disposeBag,
+            onSuccess: {_ in
+            // do some thing
+
+        }, onError: {_ in
+            // error handling
+
+        })
+
     }
 
 }
-
--- AlamofireSample/WeblandApi/TradeListRequest.swift
+++ AlamofireSample/WeblandApi/TradeListRequest.swift
@@ -15,37 +15,43 @@ import Alamofire
 // MARK: - Request
 enum TradeListRequest: WeblandApiBaseRequestProtocol {
     typealias ResponseType = TradeListResponse
-
+//    case get(
+//        _ type: String?,                   // 取引の種類
+//        _ region: String?,                 // 地区
+//        _ municipalityCode: String?,       // 市区町村コード
+//        _ prefecture: String?,             // 都道府県名
+//        _ municipality: String?,           // 市区町村名
+//        _ districtName: String?,           // 地区名
+//        _ nearestStation: String?,         // 最寄駅：名称
+//        _ timeToNearestStation: String?,   // 最寄駅：距離（分）
+//        _ tradePrice: String?,             // 取引価格（総額）
+//        _ pricePerUnit: String?,           // 坪単価(only japanese)
+//        _ floorPlan: String?,              // 間取り
+//        _ area: String?,                   // 面積（平方メートル）
+//        _ unitPrice: String?,              // 取引価格（平方メートル単価）
+//        _ landShape: String?,              // 土地の形状
+//        _ frontage: String?,               // 間口
+//        _ totalFloorArea: String?,         // 延床面積(平方メートル)
+//        _ buildingYear: String?,           // 建築年
+//        _ structure: String?,              // 建物の構造
+//        _ use: String?,                    // 用途
+//        _ purpose: String?,                // 今後の利用目的
+//        _ direction: String?,              // 前面道路：方位
+//        _ classification: String?,         // 前面道路：種類
+//        _ breadth: String?,                // 前面道路：幅員（ｍ）
+//        _ cityPlanning: String?,           // 都市計画
+//        _ coverageRatio: String?,          // 建ぺい率（％）
+//        _ floorAreaRatio: String?,         // 容積率（％）
+//        _ period: String?,                 // 取引時点
+//        _ renovation: String?,             // 改装
+//        _ remarks: String?                 // 取引の事情等
+//    )
     case get(
-        type: String,                   // 取引の種類
-        region: String,                 // 地区
-        municipalityCode: String,       // 市区町村コード
-        prefecture: String,             // 都道府県名
-        municipality: String,           // 市区町村名
-        districtName: String,           // 地区名
-        nearestStation: String,         // 最寄駅：名称
-        timeToNearestStation: String,   // 最寄駅：距離（分）
-        tradePrice: String,             // 取引価格（総額）
-        pricePerUnit: String,           // 坪単価(only japanese)
-        floorPlan: String,              // 間取り
-        area: String,                   // 面積（平方メートル）
-        unitPrice: String,              // 取引価格（平方メートル単価）
-        landShape: String,              // 土地の形状
-        frontage: String,               // 間口
-        totalFloorArea: String,         // 延床面積(平方メートル)
-        buildingYear: String,           // 建築年
-        structure: String,              // 建物の構造
-        use: String,                    // 用途
-        purpose: String,                // 今後の利用目的
-        direction: String,              // 前面道路：方位
-        classification: String,         // 前面道路：種類
-        breadth: String,                // 前面道路：幅員（ｍ）
-        cityPlanning: String,           // 都市計画
-        coverageRatio: String,          // 建ぺい率（％）
-        floorAreaRatio: String,         // 容積率（％）
-        period: String,                 // 取引時点
-        renovation: String,             // 改装
-        remarks: String                 // 取引の事情等
+        from: String?,                  // 取引時期From
+        to: String?,                    // 取引時期To
+        area: String?,                  // 都道府県コード
+        city: String?,                  // 市区町村コード
+        station: String?                // 駅コード
     )
     //case get(keyword: String, ids: [Int])
 
@@ -58,7 +64,7 @@ enum TradeListRequest: WeblandApiBaseRequestProtocol {
     var path: String {
         return WeblandApiConstants.tradeListSearch.path
     }
-    
+
     var parameters: Parameters? {
         switch self {
             case .get:
